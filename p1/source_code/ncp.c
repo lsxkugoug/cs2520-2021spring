0a78312cba5f125157cf74fddf21d7601ab8bd59
lsxkugoug <79183378+lsxkugoug@users.noreply.github.com>
2021-09-23 15:38:02 -0400

    1

--- p1/source_code/ncp.c
+++ p1/source_code/ncp.c
@@ -21,6 +21,7 @@ int main(int argc, char **argv) { /* udp related */
     int sock;
     fd_set mask;
     fd_set read_mask;
+
     int bytes;
     int num;
 
@@ -28,10 +29,10 @@ int main(int argc, char **argv) { /* udp related */
     uhdr *hdr = (uhdr *) mess_buf;
     char *data_buf = &mess_buf[sizeof(uhdr)];
 
-    int seq ;
+    int seq;
     struct timeval last_recv_time = {0, 0};
-    struct timeval nowtime = {0,0};
-    struct timeval        timeout;
+    struct timeval nowtime = {0, 0};
+    struct timeval timeout;
     char window[WINDOW_SIZE][MAX_MESS_LEN];
     int head = 1;
     int tail = WINDOW_SIZE;
@@ -65,7 +66,7 @@ int main(int argc, char **argv) { /* udp related */
     int fd = open(Source_file_name, O_RDONLY | O_CREAT);
     FD_ZERO(&read_mask);
     FD_SET(sock, &read_mask);
-    FD_SET(fd, &read_mask);
+    FD_SET(fd,&read_mask);
 
     /* 0： have not transmitted the filename to receiver
      * 1:  Already transmitted the filename to receiver
@@ -80,113 +81,123 @@ int main(int argc, char **argv) { /* udp related */
      * Secondly, Read data from file and fill in the Window ,then send the whole window */
     strcpy(data_buf, Source_file_name);
     hdr->seq = seq++;
-    sendto(sock, mess_buf, sizeof(uhdr)+strlen(data_buf), 0, 
-                       (struct sockaddr *)&send_addr, sizeof(send_addr));
-    for(int i = head;i<=tail;i++){
+    sendto(sock, mess_buf, sizeof(uhdr) + strlen(data_buf), 0,
+           (struct sockaddr *) &send_addr, sizeof(send_addr));
+    for (int i = head; i <= tail; i++) {
         bytes = read(fd, data_buf, sizeof(mess_buf) - sizeof(uhdr));
         hdr->seq = seq++;
         // Send Message
-        sendto(sock, mess_buf, sizeof(uhdr)+strlen(data_buf), 0, 
-                       (struct sockaddr *)&send_addr, sizeof(send_addr));
+        sendto(sock, mess_buf, sizeof(uhdr) + strlen(data_buf), 0,
+               (struct sockaddr *) &send_addr, sizeof(send_addr));
         // Store Message in window
-        memcpy(window[i%WINDOW_SIZE],mess_buf,sizeof(mess_buf));
+        memcpy(window[i % WINDOW_SIZE], mess_buf, sizeof(mess_buf));
     }
 
     for (;;) {
 
         /* (Re)set mask and timeout */
         mask = read_mask;
+
         timeout.tv_sec = 1;
         timeout.tv_usec = 0;
 
-	    /* Wait for message or timeout */
+        /* Wait for message or timeout */
         num = select(FD_SETSIZE, &mask, NULL, NULL, &timeout);
+        printf("Num,%d , EOF %d \n",num,eof);
 
-        if(num>0){ 
-	        /* Receive Response from Receiver */
-          if (FD_ISSET(sock, &mask)) {
-              from_len = sizeof(from_addr);
-              bytes = recvfrom(sock, mess_buf, sizeof(mess_buf), 0,
-                          (struct sockaddr *)&from_addr, 
-                          &from_len);
-              from_ip = from_addr.sin_addr.s_addr;
-              printf("Receive ACK from receiver:%d, head:%d,tail:%d\n",hdr->cack,head,tail);
+        /* No Response From Receiver */
 
-              /* Check whether we finish sending the file*/
-              if (hdr->cack >= tail ||head>=tail ){
-                  printf("Finish Sending the file ....");
-                  close(fd);
-                  exit(0);
-              }
+        /* Receive Response from Receiver  (num = 1  eof=1)=> receiver  num==0 No receiver, No file  */
+        /* (eof==1&&num==1)  num= 1 eof = 0*/
+        if(num==2){
+            if (FD_ISSET(sock, &mask)) {
+                from_len = sizeof(from_addr);
+                bytes = recvfrom(sock, mess_buf, sizeof(mess_buf), 0,
+                                 (struct sockaddr *) &from_addr,
+                                 &from_len);
+                from_ip = from_addr.sin_addr.s_addr;
+                printf("Receive ACK from receiver:%d, head:%d,tail:%d\n", hdr->cack, head, tail);
 
-              int received_ack = hdr->cack;
+                /* Check whether we finish sending the file*/
+                if (eof == 1 && (hdr->cack >= tail || head >= tail)) {
+                    printf("Finish Sending the file ....");
+                    close(fd);
+                    exit(0);
+                }
 
-              /* When Cumulative ACK is below than head,We may lose some packet. Check Nack array,and Resend Message */
-              if(received_ack < head){
-                  for(int i =0;i<NACK_SIZE;i++){
-                      if(hdr->nack[i]<0){
-                          break;
-                      }else{
-                          if(hdr->nack[i]>=head&&hdr->nack[i]<=tail ) {
-                              sendto(sock, window[hdr->nack[i] % WINDOW_SIZE],
-                                     sizeof(window[hdr->nack[i] % WINDOW_SIZE]), 0,
-                                     (struct sockaddr *) &send_addr, sizeof(send_addr));
-                          }
-                      }
-                  }
-              }
-              /* Window [head, head+1.....tail-1,tail] */
-              /* When Cumulative ACK is above head-1, shift window,send new message */
-              else if(received_ack>head-1){
-                  if(eof==1){
-                      /* Nothing to read, Just move the head */
-                      head = received_ack+1 ;
-                  }else {
-                      /* Otherwise, shit window, increase head to Received ack+1
-                       * Meanwhile, we increase tail and read from file into buffer and send them to Receiver
-                       * */
-                      for (int i = head; eof!=1 && i <= received_ack + 1; i++) {
+                int received_ack = hdr->cack;
 
-                          bytes = read(fd, data_buf, sizeof(mess_buf) - sizeof(uhdr));
+                /* When Cumulative ACK is below than head,We may lose some packet. Check Nack array,and Resend Message */
+                if (received_ack < head) {
+                    for (int i = 0; i < NACK_SIZE; i++) {
+                        if (hdr->nack[i] < 0) {
+                            break;
+                        } else {
+                            if (head <= hdr->nack[i] && hdr->nack[i] <= tail) {
+                                sendto(sock, window[hdr->nack[i] % WINDOW_SIZE],
+                                       sizeof(window[hdr->nack[i] % WINDOW_SIZE]), 0,
+                                       (struct sockaddr *) &send_addr, sizeof(send_addr));
+                            }
+                        }
+                    }
+                }
+                    /* Window [head, head+1.....tail-1,tail] */
+                    /* When Cumulative ACK is above head-1, shift window,send new message */
+                else if (received_ack >= head) {
+                    if (eof == 1) {
+                        /* Nothing to read, Just move the head */
+                        head = received_ack + 1;
+                    } else {
+                        /* Otherwise, shit window, increase head to Received ack+1
+                         * Meanwhile, we increase tail and read from file into buffer and send them to Receiver
+                         * */
+                        for (int i = head;  i < received_ack + 1; i++) {
 
-                          // Check whether Read the end of the file
-                          if (bytes < sizeof(mess_buf) - sizeof(uhdr)) {
-                              printf("Finished Reading .\n");
-                              eof = 1;
-                          }
+                            bytes = read(fd, data_buf, sizeof(mess_buf) - sizeof(uhdr));
 
-                          // Store Message in window sizeof(mess_buf) vs sizeof(uhdr)+strlen(data_buf)
-                          memcpy(window[(tail + 1) % WINDOW_SIZE], mess_buf, sizeof(mess_buf));
+                            // Check whether Read the end of the file
+                            if (bytes < sizeof(mess_buf) - sizeof(uhdr)) {
+                                printf("Finished Reading .\n");
+                                eof = 1;
+                               // close(fd);
+                            }
+                            hdr->seq = seq++;
 
-                          hdr->seq = seq++;
+                            // Store Message in window sizeof(mess_buf) vs sizeof(uhdr)+strlen(data_buf)
+                            memcpy(window[(tail + 1) % WINDOW_SIZE], mess_buf, sizeof(mess_buf));
 
-                          // Send Message
-                          sendto(sock, mess_buf, sizeof(uhdr) + strlen(data_buf), 0,
-                                 (struct sockaddr *) &send_addr, sizeof(send_addr));
+                            // Send Message
+                            sendto(sock, mess_buf, sizeof(uhdr) + strlen(data_buf), 0,
+                                   (struct sockaddr *) &send_addr, sizeof(send_addr));
 
-                          head++;
-                          tail++;
+                            head++;
+                            tail++;
 
-                          // Reset the Timer
-                          timeout.tv_sec = 1;
-                          timeout.tv_usec = 0;
-                      }
-                  }
-              }          
+                            // Reset the Timer
+                            timeout.tv_sec = 1;
+                            timeout.tv_usec = 0;
+
+                            if(eof ==1 ){
+                                head = received_ack+1;
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }else{
-            if(head > tail){
+            if (head == tail) {
                 close(fd);
                 exit(0);
             }
             /* Time out, Resend everything in buffer to Receiver */
-            printf("Time out, Resend the whole window!");
-            for(int i = head;i<=tail;i++) {
+            for (int i = head; i <= tail; i++) {
                 // Send Message
                 sendto(sock, window[i % WINDOW_SIZE], sizeof(window[i % WINDOW_SIZE]), 0,
                        (struct sockaddr *) &send_addr, sizeof(send_addr));
             }
         }
+
     }
     return 0;
 }
