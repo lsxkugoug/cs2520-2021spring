73e83b90b76b66e6346d083747de2dc8a1c66841
lsxkugoug <79183378+lsxkugoug@users.noreply.github.com>
2021-09-22 22:37:02 -0400

    rebuild

--- p1/tutorial_code/testAPI.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-main(){
-    char s[] = "filename@ip:1234";
-    char *delim = "@:";
-    char *p;
-    printf("%s\n", strtok(s, delim));
-    printf("%s\n", strtok(NULL, delim));
-    printf("%s\n", strtok(NULL, delim));
-}
