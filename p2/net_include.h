b640d093d1b964df81f136509afa0b1eb40b2eca
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2021-10-07 10:52:18 -0400

    Add files via upload

--- p2/net_include.h
+++ p2/net_include.h
@@ -14,19 +14,19 @@
 #include<sys/stat.h>
 #include <errno.h>
 
-#define MAX_MESS_LEN 4000
-#define WINDOW_SIZE 200
+#define MAX_MESS_LEN 1500
 #define NACK_SIZE 3
 #define RECORD_SIZE 50
 #define TMB 10000000
 
 typedef struct dummy_uhdr {
-    /* 0-> Sender sends data to Receiver
-     * 1->Sender sends ACKACK
-     * 2->Sender Receive ACK from Receiver
+    /* 0->Sender sends data to Receiver
+     * 1->Sender sends ACKACK to Receiver
+     * 2->Receiver sends ACK to Sender.
      * 3->Receiver sends request
      * 4->Sender sends permission
      * 5-> Sender sends decline
+     * 6 -> Sender sends data to Receiver, but it is request from receiver (NACK)
      * */
     int     type;
 
@@ -34,14 +34,13 @@ typedef struct dummy_uhdr {
 
     int32_t ack;
     int32_t nack[NACK_SIZE];
-    int32_t ackack;
 
-    struct timeval DeliverTime; /*The delivery time of data*/
-    struct timeval Send_TS;     /*Send time of packet. It could change when we resend the Packet*/
-    struct timeval Receive_TS1; /*First time receiver receives the packet*/
-    struct timeval ACKACK_TS;   /*The time when sender gets the ACK and send ACKACK*/
+    struct timeval Send_TS;      /*Sending time of packet.*/
+    struct timeval N_Send_TS;    /*Resending time of packet(Response to NACK)*/
+    struct timeval Receive_TS1;  /*First time receiver receives the packet*/
+    struct timeval ACKACK_TS;    /*The time when sender gets the ACK and send ACKACK*/
 
-    struct timeval base_delta;  /*Measure of ½ RTT+ clock _diff*/
-    struct timeval rtt;         /*Measure of RTT*/
+    int     WindowSize;           /* Use for Sender initial its*/
+    struct timeval Halfrtt;       /*Measure of RTT*/
 
 } uhdr;
